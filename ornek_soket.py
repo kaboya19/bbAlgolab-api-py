88fc5727532e40fe71b48435300c961cd42f2b35
Bora Kaya <kayabo19@itu.edu.tr>
2024-03-16 23:51:50 +0300

    .

--- ornek_soket.py
+++ ornek_soket.py
@@ -1,18 +1,81 @@
+import sqlite3
+import json
 from algolab import API
 from ws import AlgoLabSocket
-import json,time
 from config import *
+import time
+
+# SQLite veritabanı dosyasının adı
+db_file = "veriler1.db"
+
+# Veritabanı bağlantısını oluştur
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+
+# Veritabanında tabloyu oluştur
+cursor.execute('''CREATE TABLE IF NOT EXISTS veriler (
+                    id INTEGER PRIMARY KEY,
+                    symbol TEXT,
+                    market TEXT,
+                    price REAL,
+                    change REAL,
+                    ask REAL,
+                    bid REAL,
+                    date TEXT,
+                    change_percentage REAL,
+                    high REAL,
+                    low REAL,
+                    trade_quantity REAL,
+                    direction TEXT,
+                    ref_price REAL,
+                    balance_price REAL,
+                    balance_amount REAL,
+                    buying TEXT,
+                    selling TEXT
+                )''')
+
+# Veritabanı bağlantısını kapat
+conn.commit()
+conn.close()
 
 def process_msg(msg):
     try:
-        t = msg["type"]
-        content = msg["content"]
-        print("Type: " + t +"Content: " + content)
+        content = msg["Content"]
+             
+        symbol = content["Symbol"]
+        market = content["Market"]
+        price = content["Price"]
+        change = content["Change"]
+        ask = content["Ask"]
+        bid = content["Bid"]
+        date = content["Date"]
+        change_percentage = content["ChangePercentage"]
+        high = content["High"]
+        low = content["Low"]
+        trade_quantity = content["TradeQuantity"]
+        direction = content["Direction"]
+        ref_price = content["RefPrice"]
+        balance_price = content["BalancePrice"]
+        balance_amount = content["BalanceAmount"]
+        buying = content["Buying"]
+        selling = content["Selling"]
+        if symbol=="EREGL":                
+           conn = sqlite3.connect(db_file)
+           cursor = conn.cursor()
+           cursor.execute('''INSERT INTO veriler (
+                                symbol, market, price, change, ask, bid, date, change_percentage,
+                                high, low, trade_quantity, direction, ref_price, balance_price,
+                                balance_amount, buying, selling
+                                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                                (symbol, market, price, change, ask, bid, date, change_percentage,
+                                high, low, trade_quantity, direction, ref_price, balance_price,
+                                balance_amount, buying, selling))
+           conn.commit()
+           conn.close()
     except Exception as e:
         print("Error processing message: ", e)
 
 if __name__ == "__main__":
-
     algo = API(api_key=MY_API_KEY, username=MY_USERNAME, password=MY_PASSWORD, auto_login=True)
     soket = AlgoLabSocket(algo.api_key, algo.hash, "T")
     soket.connect()
@@ -21,16 +84,13 @@ if __name__ == "__main__":
 
     data = {"Type": "T", "Symbols": ["ALL"]}
     soket.send(data)
-
-    i = 0
     while soket.connected:
         data = soket.recv()
-        i += 1
         if data:
             try:
                 msg = json.loads(data)
-                print(msg)
+                process_msg(msg)
             except:
-                print("error 1")
+                print("Error processing message")
                 soket.close()
                 break
